f1e943bd90332862007d225cf7901019dcdaee89
Phạm Lê Tuấn Anh <119119752+tale05@users.noreply.github.com>
2024-05-27 17:02:35 +0700

    a

--- WebAPIStrain/obj/Debug/net8.0/WebAPIStrain.AssemblyInfo.cs
+++ WebAPIStrain/obj/Debug/net8.0/WebAPIStrain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bb74c475520168b2be92591f3b5cbeb806f932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0fa04d9e4b901b604d83e27040547ade43f63d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPIStrain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
